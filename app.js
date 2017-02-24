10b3a50bbaed2612a55deb2cc1791785207cb373
Achilles Dinglasan <adinglasan@reekoh.com>
2017-02-24 17:57:20 +0800

    migrated to v3

--- app.js
+++ app.js
@@ -1,58 +1,53 @@
-'use strict';
+'use strict'
 
-var async         = require('async'),
-	isArray       = require('lodash.isarray'),
-	platform      = require('./platform'),
-	isPlainObject = require('lodash.isplainobject'),
-	keenClient, collection;
+let reekoh = require('reekoh')
+let _plugin = new reekoh.plugins.Connector()
+let async = require('async')
+let isArray = require('lodash.isarray')
+let isPlainObject = require('lodash.isplainobject')
+let keenClient = null
 
-let sendData = function (data, callback) {
-	keenClient.addEvent(collection, data, function (error) {
-        if(!error) {
-            platform.log(JSON.stringify({
-                title: 'Added Keen.io Data',
-                collection: collection,
-                data: data
-            }));
-        }
+let sendData = (data, callback) => {
+  keenClient.addEvent(_plugin.config.collection, data, (error) => {
+    if (!error) {
+      _plugin.log(JSON.stringify({
+        title: 'Added Keen.io Data',
+        collection: _plugin.config.collection,
+        data: data
+      }))
+    }
+    callback(error)
+  })
+}
 
-        callback(error);
-	});
-};
+_plugin.on('data', (data) => {
+  if (isPlainObject(data)) {
+    sendData(data, (error) => {
+      if (error) _plugin.logException(error)
+    })
+  } else if (isArray(data)) {
+    async.each(data, (datum, done) => {
+      sendData(datum, done)
+    }, (error) => {
+      if (error) _plugin.logException(error)
+    })
+  } else {
+    _plugin.logException(new Error('Invalid data received. Must be a valid Array/JSON Object. Data ' + data))
+  }
+})
 
-platform.on('data', function (data) {
-	if (isPlainObject(data)) {
-		sendData(data, (error) => {
-            if (error) platform.handleException(error);
-		});
-	}
-	else if (isArray(data)) {
-		async.each(data, (datum, done) => {
-			sendData(datum, done);
-		}, (error) => {
-            if (error) platform.handleException(error);
-        });
-	}
-	else
-		platform.handleException(new Error('Invalid data received. Must be a valid Array/JSON Object. Data ' + data));
-});
+_plugin.once('ready', () => {
+  let Keen = require('keen-js')
 
-platform.on('close', function () {
-	platform.notifyClose(); // No resources to clean up. Just notify the platform.
-});
+  keenClient = new Keen({
+    projectId: _plugin.config.projectId,
+    writeKey: _plugin.config.writeKey,
+    protocol: 'https',
+    requestType: 'jsonp'
+  })
 
-platform.once('ready', function (options) {
-	collection = options.collection;
+  _plugin.log('Keen Connector has been initialized.')
+  _plugin.emit('init')
+})
 
-	var Keen = require('keen-js');
-
-	keenClient = new Keen({
-		projectId: options.project_id,
-		writeKey: options.write_key,
-		protocol: 'https',
-		requestType: 'jsonp'
-	});
-
-	platform.log('Keen.io Connector Initialized.');
-	platform.notifyReady();
-});
+module.exports = _plugin
--- test/keen-connector.test.js
+++ test/keen-connector.test.js
@@ -1,85 +1,64 @@
-'use strict';
+'use strict'
 
-const PROJECT_ID = '560db78dd2eaaa7968e3229b',
-	  WRITE_KEY  = 'c9d25c945c7b15dee078343a593f41f6761be5c25ef99253c5b4628042b3cb0799cdf64d0e0ac79607a5fd4eff921c86932b3240d20db12613abbc7f363beffb4a6e35563d14eed991dd41408b33900b88c80e80849bdc1eea4ae8b4194929b587c0d3ded06d42a1ad34b928dd774600',
-	  COLLECTION = 'reekoh-data';
+const amqp = require('amqplib')
 
-var cp     = require('child_process'),
-	should = require('should'),
-	connector;
+const PROJECT_ID = '560db78dd2eaaa7968e3229b'
+const WRITE_KEY  = 'c9d25c945c7b15dee078343a593f41f6761be5c25ef99253c5b4628042b3cb0799cdf64d0e0ac79607a5fd4eff921c86932b3240d20db12613abbc7f363beffb4a6e35563d14eed991dd41408b33900b88c80e80849bdc1eea4ae8b4194929b587c0d3ded06d42a1ad34b928dd774600'
+const COLLECTION = 'reekoh-data'
 
-describe('Connector', function () {
-	this.slow(8000);
+let _channel = null
+let _conn = null
+let app = null
 
-	after('terminate child process', function (done) {
-		this.timeout(5000);
+describe('Keen Connector Test', () => {
+  before('init', () => {
+    process.env.ACCOUNT = 'adinglasan'
+    process.env.CONFIG = JSON.stringify({
+      projectId: PROJECT_ID,
+      writeKey: WRITE_KEY,
+      collection: COLLECTION
+    })
+    process.env.INPUT_PIPE = 'ip.keen'
+    process.env.LOGGERS = 'logger1, logger2'
+    process.env.EXCEPTION_LOGGERS = 'ex.logger1, ex.logger2'
+    process.env.BROKER = 'amqp://guest:guest@127.0.0.1/'
 
-		setTimeout(function () {
-			connector.kill('SIGKILL');
-			done();
-		}, 4000);
-	});
+    amqp.connect(process.env.BROKER)
+      .then((conn) => {
+        _conn = conn
+        return conn.createChannel()
+      }).then((channel) => {
+      _channel = channel
+    }).catch((err) => {
+      console.log(err)
+    })
+  })
 
-	describe('#spawn', function () {
-		it('should spawn a child process', function () {
-			should.ok(connector = cp.fork(process.cwd()), 'Child process not spawned.');
-		});
-	});
+  after('close connection', function (done) {
+    _conn.close()
+    done()
+  })
 
-	describe('#handShake', function () {
-		it('should notify the parent process when ready within 5 seconds', function (done) {
-			this.timeout(8000);
+  describe('#start', function () {
+    it('should start the app', function (done) {
+      this.timeout(10000)
+      app = require('../app')
+      app.once('init', done)
+    })
+  })
 
-			connector.on('message', function (message) {
-				if (message.type === 'ready')
-					done();
-			});
+  describe('#data', () => {
+    it('should send data to third party client', function (done) {
+      this.timeout(15000)
 
-			connector.send({
-				type: 'ready',
-				data: {
-					options: {
-						project_id: PROJECT_ID,
-						write_key: WRITE_KEY,
-						collection: COLLECTION
-					}
-				}
-			}, function (error) {
-				should.ifError(error);
-			});
-		});
-	});
+      let data = {
+        key1: 'value1',
+        key2: 121,
+        key3: 40
+        }
 
-	describe('#data', function (done) {
-		it('should process and send the JSON data to the keen.io collection', function () {
-			connector.send({
-				type: 'data',
-				data: {
-					key1: 'value1',
-					key2: 121,
-					key3: 40
-				}
-			}, done);
-		});
-	});
-
-	describe('#data', function (done) {
-		it('should process and send the Array data to the keen.io collection', function () {
-			connector.send({
-				type: 'data',
-				data: [
-					{
-						key1: 'value1',
-						key2: 121,
-						key3: 40
-					},
-					{
-						key1: 'value1',
-						key2: 121,
-						key3: 40
-					}
-				]
-			}, done);
-		});
-	});
-});
+      _channel.sendToQueue('ip.keen', new Buffer(JSON.stringify(data)))
+      setTimeout(done, 10000)
+    })
+  })
+})
